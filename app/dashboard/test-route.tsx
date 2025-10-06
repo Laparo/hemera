c7cc217b14305ef51ea5cd2b39493336bd98111e
Andreas Bernt-Baertl <abb@laparo.biz>
2025-10-06 21:38:14 +0200

    Add test routes to debug routing issue

---
+++ app/dashboard/test-route.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material';
+
+export default function TestPage() {
+  return (
+    <div>
+      <Typography variant='h1'>Test Dashboard Route</Typography>
+      <Typography>This is a test to verify route resolution works</Typography>
+    </div>
+  );
+}
---
+++ app/simple-dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material';
+
+export default function SimpleDashboard() {
+  return (
+    <div>
+      <Typography variant='h1'>Simple Dashboard</Typography>
+      <Typography>This route should work without any auth</Typography>
+    </div>
+  );
+}
